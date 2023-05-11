936056d70513cc455213f0b3ee1ed7bd1023ced3
mighty-mhsl <mihusle@protonmail.com>
2023-05-11 13:33:52 +0200

    Introduced user context filter for constant parsing of x-user-id http header

---
+++ src/main/java/faang/school/postservice/config/context/UserContext.java
@@ -0,0 +1,21 @@
+package faang.school.postservice.config.context;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserContext {
+
+    private final ThreadLocal<Long> userIdHolder = new ThreadLocal<>();
+
+    public void setUserId(long userId) {
+        userIdHolder.set(userId);
+    }
+
+    public long getUserId() {
+        return userIdHolder.get();
+    }
+
+    public void clear() {
+        userIdHolder.remove();
+    }
+}

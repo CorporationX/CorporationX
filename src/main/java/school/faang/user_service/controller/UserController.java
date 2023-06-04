d06e87cd933da3fa42a83067aa8fdb40fb071007
InnMish <inn.mishustina@gmail.com>
2023-06-04 18:53:04 +0300

    imports

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -1,19 +1,26 @@
 package school.faang.user_service.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
 import school.faang.user_service.dto.user.UserProfileDto;
 import school.faang.user_service.service.UserService;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor

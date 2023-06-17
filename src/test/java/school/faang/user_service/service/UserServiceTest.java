3eb861abe070bdcac88f2b3d77377fe07acac22e
Inna Mishustina <52246032+InnMish@users.noreply.github.com>
2023-06-17 22:25:12 +0300

    Feature/ci pipeline (#12)
    ci-pipeline distribution

---
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void getUsers() {
+        assertTrue(true);
+    }
+}

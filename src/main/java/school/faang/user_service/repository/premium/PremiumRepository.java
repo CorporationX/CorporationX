9a734d9c9d9eaae3008a0b346a4a2de793f8c6d3
mighty-mhsl <mihusle@protonmail.com>
2023-07-15 16:18:32 +0200

    Added outdated Premium entities fetching method

--- src/main/java/school/faang/user_service/repository/premium/PremiumRepository.java
+++ src/main/java/school/faang/user_service/repository/premium/PremiumRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.premium.Premium;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface PremiumRepository extends CrudRepository<Premium, Long> {
 
     boolean existsByUserId(long userId);
+
+    List<Premium> findAllByEndDateBefore(LocalDateTime endDate);
 }

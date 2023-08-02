f6af6b84e9fd4fd07b8efca1fd935ff2e07bd88b
RasulMurtuzaliev <ras8705@mail.ru>
2023-08-02 14:51:21 +0500

    created controller, service and dto

---
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -0,0 +1,10 @@
+package faang.school.postservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+}

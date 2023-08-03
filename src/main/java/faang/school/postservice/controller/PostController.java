4ec3252f25aebffd084d42500d2cac3e011fda97
RasulMurtuzaliev <ras8705@mail.ru>
2023-08-03 15:23:03 +0500

    created exceptions, PostMapper interface refactoring PostController, PostDto classes updated PostService class

--- src/main/java/faang/school/postservice/controller/PostController.java
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -1,14 +1,22 @@
 package faang.school.postservice.controller;
 
+import faang.school.postservice.dto.PostDto;
 import faang.school.postservice.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/api/v1/posts")
 public class PostController {
     private final PostService postService;
 
+    @PostMapping
+    public PostDto crateDraftPost(@RequestBody @Validated PostDto postDto) {
+        return postService.createDraftPost(postDto);
+    }
 }
--- src/main/java/faang/school/postservice/dto/PostDto.java
+++ src/main/java/faang/school/postservice/dto/PostDto.java
@@ -1,5 +1,7 @@
 package faang.school.postservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostDto {
     private Long id;
+
+    @NotBlank(message = "Content is required")
     private String content;
+
     private Long authorId;
     private Long projectId;
 }

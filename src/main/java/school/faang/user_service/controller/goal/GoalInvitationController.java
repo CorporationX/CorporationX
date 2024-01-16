d3a7654260525add46dd80d5b70432747acd5376
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-01-16 19:40:59 +0300

    editing http method

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -2,7 +2,7 @@ package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
@@ -18,7 +18,7 @@ public class GoalInvitationController {
 
     private final GoalInvitationService goalInvitationService;
 
-    @PostMapping(name = "/accept/{id}")
+    @PutMapping(name = "/accept/{id}")
     public ResponseEntity<GoalInvitationDto> acceptGoalInvitation(long id) {
         goalInvitationService.acceptGoalInvitation(id);
         return ResponseEntity.ok().build();

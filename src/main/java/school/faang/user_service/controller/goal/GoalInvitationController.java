6ef988526f03b2eb6375485c664753f1facdbbee
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-01-17 17:09:09 +0300

    Add fix controller to task BJS2-1569

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -2,6 +2,7 @@ package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
     @PutMapping("/accept/{id}")
-    public ResponseEntity<GoalInvitationDto> acceptGoalInvitation(long id) {
+    public ResponseEntity<GoalInvitationDto> acceptGoalInvitation(@PathVariable long id) {
         goalInvitationService.acceptGoalInvitation(id);
         return ResponseEntity.ok().build();
     }

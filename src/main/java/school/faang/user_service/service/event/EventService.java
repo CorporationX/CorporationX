b43c0fc7f3b9fc70f38a9bdd3c36f53671f286b3
Vlad Meniailov <vlad.meniailov@yandex.ru>
2024-01-17 20:58:35 +0300

    Fixed after Inna's review.

--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -2,55 +2,23 @@ package school.faang.user_service.service.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.dto.event.EventDto;
-import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.exception.DataValidationException;
-import school.faang.user_service.mapper.event.EventMapper;
-import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.event.EventRepository;
-
-import java.util.HashSet;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
-    private final EventMapper eventMapper;
-    private final UserRepository userRepository;
-
-    public EventDto create(EventDto event) {
-        Event eventEntity = eventMapper.toEntity(event);
-        validateOwnerHasSkills(eventEntity);
-        Event savedEvent = eventRepository.save(eventEntity);
-        event.setId(savedEvent.getId());
-        return event;
-    }
 
     public void deleteEvent(long eventId) {
-        Event event = validateAndGetEvent(eventId);
+        Event event = getEvent(eventId);
         eventRepository.deleteById(event.getId());
     }
 
-    public Event validateAndGetEvent(long eventId) {
+    public Event getEvent(long eventId) {
         return eventRepository.findById(eventId)
                 .orElseThrow(() -> new DataValidationException("Event not found with ID - " + eventId));
-    }
-
-    public void validateOwnerHasSkills(Event event) {
-        Optional<User> owner = userRepository.findById(event.getOwner().getId());
-        User ownerById = owner
-                .orElseThrow(() -> new DataValidationException("Owner by id not found"));
-
-        if (!isOwnerHasEventSkills(event, ownerById)) {
-            throw new DataValidationException("Owner doesn't have required skill");
-        }
-    }
-
-    private boolean isOwnerHasEventSkills(Event event, User ownerById) {
-        return new HashSet<>(ownerById.getSkills())
-                .containsAll(event.getRelatedSkills());
     }
 
 }

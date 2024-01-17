c14dc3ab196c2c9edcabecb2680546f642a5a9e8
Edisolt Khatsiev <Edisolt Khatsiev>
2024-01-17 05:46:17 +0300

    Реализовать возможность ментору удалять менти. Tests upgraded.

--- src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceTest.java
@@ -4,38 +4,47 @@ import jakarta.persistence.EntityNotFoundException;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@ExtendWith(MockitoExtension.class)
 public class MentorshipServiceTest {
     @Mock
     private MentorshipRepository mentorshipRepository;
+    @Mock
+    private UserRepository userRepository;
     @InjectMocks
     private MentorshipService mentorshipService;
     private static final long EXISTENT_MENTEE_ID = 1L;
     private static final long EXISTENT_MENTOR_ID = 2L;
     private static final long NON_EXISTENT_USER_ID = 100_000L;
+    private User mentor;
+    private User mentee;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        mentor = User.builder().id(EXISTENT_MENTOR_ID).build();
+        mentee = User.builder().id(EXISTENT_MENTEE_ID).mentors(List.of(mentor)).build();
+        User anotherMentee = User.builder().id(3L).build();
+        mentor.setMentees(new ArrayList<>(List.of(mentee, anotherMentee)));
     }
 
     @Test
-    public void testDeleteMentee_EntityNotFoundException() {
-        Mockito.when(mentorshipRepository.findById(NON_EXISTENT_USER_ID)).thenReturn(Optional.empty());
-        Mockito.when(mentorshipRepository.findById(EXISTENT_MENTOR_ID)).thenReturn(Optional.of(new User()));
-        Mockito.when(mentorshipRepository.findById(EXISTENT_MENTEE_ID)).thenReturn(Optional.of(new User()));
+    public void testDeleteMentee_AnyUserNotExist_EntityNotFoundException() {
+        Mockito.when(userRepository.findById(NON_EXISTENT_USER_ID)).thenReturn(Optional.empty());
 
         Assert.assertThrows(
                 EntityNotFoundException.class,
@@ -52,19 +61,9 @@ public class MentorshipServiceTest {
     }
 
     @Test
-    public void testGetMentees_EntityFound() {
-
-        User menteeToDelete = new User();
-        User mentee = new User();
-        User mentor = new User();
-
-        menteeToDelete.setId(5L);
-        mentee.setId(6L);//for the equals and remove method to work correctly
-        menteeToDelete.setMentors(List.of(mentor));
-        mentor.setMentees(new ArrayList<>(List.of(mentee, menteeToDelete)));
-
-        Mockito.when(mentorshipRepository.findById(EXISTENT_MENTEE_ID)).thenReturn(Optional.of(menteeToDelete));
-        Mockito.when(mentorshipRepository.findById(EXISTENT_MENTOR_ID)).thenReturn(Optional.of(mentor));
+    public void testDeleteMentee_UsersExistsWithMentorship_MenteeWasDeletedAndMentorSaved() {
+        Mockito.when(userRepository.findById(EXISTENT_MENTEE_ID)).thenReturn(Optional.of(mentee));
+        Mockito.when(userRepository.findById(EXISTENT_MENTOR_ID)).thenReturn(Optional.of(mentor));
 
         List<User> menteesOfMentor = mentor.getMentees();
         int menteesBeforeRemovalCount = menteesOfMentor.size();
@@ -75,10 +74,34 @@ public class MentorshipServiceTest {
 
         assertEquals(2, menteesBeforeRemovalCount);
         assertEquals(1, menteesAfterRemovalCount);
-        assertFalse(menteesOfMentor.contains(menteeToDelete));
-        assertTrue(menteesOfMentor.contains(mentee));
+        assertFalse(menteesOfMentor.contains(mentee));
 
-        Mockito.verify(mentorshipRepository, Mockito.times(1)).findById(EXISTENT_MENTEE_ID);
-        Mockito.verify(mentorshipRepository, Mockito.times(1)).findById(EXISTENT_MENTOR_ID);
+        Mockito.verify(userRepository, Mockito.times(1)).findById(EXISTENT_MENTEE_ID);
+        Mockito.verify(userRepository, Mockito.times(1)).findById(EXISTENT_MENTOR_ID);
+        Mockito.verify(mentorshipRepository, Mockito.times(1)).save(mentor);
+    }
+
+    @Test
+    public void testDeleteMentee_UsersExistsWithNoMentorship_MenteeWasNotDeletedAndMentorWasNotSaved() {
+        mentor.getMentees().remove(mentee);
+        //mentor NOW do not have deleting mentee
+
+        Mockito.when(userRepository.findById(EXISTENT_MENTEE_ID)).thenReturn(Optional.of(mentee));
+        Mockito.when(userRepository.findById(EXISTENT_MENTOR_ID)).thenReturn(Optional.of(mentor));
+
+        List<User> menteesOfMentor = mentor.getMentees();
+        int menteesBeforeRemovalCount = menteesOfMentor.size();
+
+        mentorshipService.deleteMentee(EXISTENT_MENTEE_ID, EXISTENT_MENTOR_ID);
+
+        int menteesAfterRemovalCount = menteesOfMentor.size();
+
+        assertEquals(1, menteesBeforeRemovalCount);
+        assertEquals(1, menteesAfterRemovalCount);
+        assertFalse(menteesOfMentor.contains(mentee));
+
+        Mockito.verify(userRepository, Mockito.times(1)).findById(EXISTENT_MENTEE_ID);
+        Mockito.verify(userRepository, Mockito.times(1)).findById(EXISTENT_MENTOR_ID);
+        Mockito.verify(mentorshipRepository, Mockito.never()).save(mentor);
     }
 }

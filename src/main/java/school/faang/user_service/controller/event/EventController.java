27e83bb396e1c8701f6ae76b7d2789dc44f99311
Vlad Meniailov <vlad.meniailov@yandex.ru>
2024-01-17 21:30:09 +0300

    Fixed "getEvent" after Inna's review.

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -3,33 +3,16 @@ package school.faang.user_service.controller.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.entity.event.Event;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.event.EventService;
-
-import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
 
-    public EventDto create(EventDto event) {
-        if (!isValidate(event)) {
-            throw new DataValidationException("Event isn't validate.");
-        }
-        return eventService.create(event);
-    }
-
     public Event getEvent(long eventId) {
         return eventService.getEvent(eventId);
-    }
-
-    public boolean isValidate(EventDto event) {
-        return (event.getTitle() != null && !event.getTitle().isEmpty())
-                && event.getStartDate().isAfter(LocalDateTime.now())
-                && event.getOwnerId() >= 0;
     }
 
 }

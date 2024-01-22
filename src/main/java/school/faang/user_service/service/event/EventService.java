ebb6ebb91d219f55864a77bedcb6b10c5b727821
Vladislav Menyaylov <vlad.meniailov@yandex.ru>
2024-01-22 15:22:42 +0300

    Fixed after last Inna's review.

--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -2,8 +2,6 @@ package school.faang.user_service.service.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import school.faang.user_service.entity.event.Event;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.event.EventRepository;
 
 @Component
@@ -12,13 +10,7 @@ public class EventService {
     private final EventRepository eventRepository;
 
     public void deleteEvent(long eventId) {
-        Event event = getEvent(eventId);
-        eventRepository.deleteById(event.getId());
-    }
-
-    public Event getEvent(long eventId) {
-        return eventRepository.findById(eventId)
-                .orElseThrow(() -> new DataValidationException("Event not found with ID - " + eventId));
+        eventRepository.deleteById(eventId);
     }
 
 }
--- src/test/java/school/faang/user_service/service/EventServiceTest.java
+++ src/test/java/school/faang/user_service/service/EventServiceTest.java
@@ -8,13 +8,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.event.Event;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.event.EventRepository;
 import school.faang.user_service.service.event.EventService;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,13 +24,13 @@ public class EventServiceTest {
     @Test
     @DisplayName("Успешное удаление события по верному Id")
     public void testSuccessDeleteEventById() {
-        Event eventDelete = Event.builder().id(5L).maxAttendees(5).build();
+        Event eventDelete = Event.builder()
+                .id(5L)
+                .maxAttendees(5)
+                .build();
         long eventId = eventDelete.getId();
 
-        Mockito.when(eventRepository.findById(eventId)).thenReturn(Optional.of(eventDelete));
-
         eventService.deleteEvent(eventId);
-
         Mockito.verify(eventRepository, times(1)).deleteById(eventId);
     }
 
@@ -42,10 +38,6 @@ public class EventServiceTest {
     @DisplayName("Неуспешное удаление события по неверному Id")
     public void testFailedDeleteEventByIncorrectId() {
         long wrongId = 15L;
-
-        Mockito.when(eventRepository.findById(wrongId)).thenReturn(Optional.empty());
-
-        assertThrows(DataValidationException.class, () -> eventService.deleteEvent(wrongId));
 
         Mockito.verify(eventRepository, Mockito.never()).deleteById(wrongId);
     }

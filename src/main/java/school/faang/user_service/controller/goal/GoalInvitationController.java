0e9d937025a62e9fe10819630fd10a06687349b1
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-01-24 22:06:42 +0300

    Fix post mapping in controller BJS2-1575

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -1,7 +1,8 @@
 package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
@@ -20,8 +21,8 @@ public class GoalInvitationController {
 
     private final GoalInvitationService goalInvitationService;
 
-    @GetMapping("/get-invitations")
-    public List<GoalInvitationDto> getInvitations(InvitationFilterDto filter) {
+    @PostMapping("/get-invitations")
+    public List<GoalInvitationDto> getInvitations(@RequestBody InvitationFilterDto filter) {
         return goalInvitationService.getInvitations(filter);
     }
 }

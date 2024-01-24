5c97751e1da9cac5d885c679929827efe536f910
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-01-24 22:19:33 +0300

    Add @Request Body in createInvitation method by controller BJS2-1431

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -2,6 +2,7 @@ package school.faang.user_service.controller.goal;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
@@ -19,7 +20,7 @@ public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
     @PostMapping("/create")
-    public GoalInvitationDto createInvitation(GoalInvitationDto invitation) {
+    public GoalInvitationDto createInvitation(@RequestBody GoalInvitationDto invitation) {
         return goalInvitationService.createInvitation(invitation);
     }
 }

ac83708b68e1766f0c21e832d006052ee6c09360
Edisolt Khatsiev <Edisolt Khatsiev>
2024-01-30 01:25:14 +0300

    JaCoCo added with 70% coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	java
-	jacoco
-	id("org.springframework.boot") version "3.0.6"
-	id("io.spring.dependency-management") version "1.1.0"
-	id("org.jsonschema2pojo") version "1.2.1"
+    java
+    jacoco
+    id("org.springframework.boot") version "3.0.6"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.jsonschema2pojo") version "1.2.1"
 }
 
 group = "faang.school"
@@ -11,131 +11,131 @@ version = "1.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	/**
-	 * Spring boot starters
-	 */
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    /**
+     * Spring boot starters
+     */
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-	/**
-	 * Database
-	 */
-	implementation("org.liquibase:liquibase-core")
-	implementation("redis.clients:jedis:4.3.2")
-	runtimeOnly("org.postgresql:postgresql")
+    /**
+     * Database
+     */
+    implementation("org.liquibase:liquibase-core")
+    implementation("redis.clients:jedis:4.3.2")
+    runtimeOnly("org.postgresql:postgresql")
 
-	/**
-	 * Amazon S3
-	 */
-	implementation("com.amazonaws:aws-java-sdk-s3:1.12.464")
+    /**
+     * Amazon S3
+     */
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.464")
 
-	/**
-	 * Utils & Logging
-	 */
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-	implementation("org.slf4j:slf4j-api:2.0.5")
-	implementation("ch.qos.logback:logback-classic:1.4.6")
-	implementation("org.projectlombok:lombok:1.18.26")
-	annotationProcessor("org.projectlombok:lombok:1.18.26")
-	implementation("org.mapstruct:mapstruct:1.5.3.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+    /**
+     * Utils & Logging
+     */
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    implementation("org.mapstruct:mapstruct:1.5.3.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.0")
 
-	/**
-	 * Test containers
-	 */
-	implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
-	testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
+    /**
+     * Test containers
+     */
+    implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
 
-	/**
-	 * Tests
-	 */
-	testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
-	testImplementation("org.assertj:assertj-core:3.24.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    /**
+     * Tests
+     */
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 jsonSchema2Pojo {
-	setSource(files("src/main/resources/json"))
-	targetDirectory = file("${project.buildDir}/generated-sources/js2p")
-	targetPackage = "com.json.student"
-	setSourceType("jsonschema")
+    setSource(files("src/main/resources/json"))
+    targetDirectory = file("${project.buildDir}/generated-sources/js2p")
+    targetPackage = "com.json.student"
+    setSourceType("jsonschema")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 val test by tasks.getting(Test::class) { testLogging.showStandardStreams = true }
 
 tasks.bootJar {
-	archiveFileName.set("service.jar")
+    archiveFileName.set("service.jar")
 }
 
 /**
  * JaCoCo settings
  */
 val jacocoInclude = listOf(
-	"**/controller/**",
-	"**/service/**",
-	"**/validator/**",
-	"**/mapper/**",
+    "**/controller/**",
+    "**/service/**",
+    "**/validator/**",
+    "**/mapper/**"
 )
 jacoco {
-	toolVersion = "0.8.9"
-	reportsDirectory.set(layout.buildDirectory.dir("$buildDir/reports/jacoco"))
+    toolVersion = "0.8.9"
+    reportsDirectory.set(layout.buildDirectory.dir("$buildDir/reports/jacoco"))
 }
 tasks.test {
-	finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport)
 }
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
+    dependsOn(tasks.test)
 
-	reports {
-		xml.required.set(false)
-		csv.required.set(false)
-		//html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-	}
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        //html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
 
-	classDirectories.setFrom(
-		sourceSets.main.get().output.asFileTree.matching {
-			include(jacocoInclude)
-		}
-	)
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            include(jacocoInclude)
+        }
+    )
 }
 tasks.jacocoTestCoverageVerification {
-	violationRules {
-		rule {
-			element = "CLASS"
-			classDirectories.setFrom(
-				sourceSets.main.get().output.asFileTree.matching {
-					include(jacocoInclude)
-				}
-			)
-			enabled = true
-			limit {
-				minimum = BigDecimal(0.7).setScale(2, BigDecimal.ROUND_HALF_UP) // Задаем минимальный уровень покрытия
-			}
-		}
-	}
+    violationRules {
+        rule {
+            element = "CLASS"
+            classDirectories.setFrom(
+                sourceSets.main.get().output.asFileTree.matching {
+                    include(jacocoInclude)
+                }
+            )
+            enabled = true
+            limit {
+                minimum = BigDecimal(0.7).setScale(2, BigDecimal.ROUND_HALF_UP) // Задаем минимальный уровень покрытия
+            }
+        }
+    }
 }

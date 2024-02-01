3c168f8001aea812903b0e58d68646ce7ef05559
InnMish <inn.mishustina@gmail.com>
2024-02-01 09:51:39 +0300

    Added resource repository

---
+++ src/main/java/faang/school/postservice/repository/ResourceRepository.java
@@ -0,0 +1,9 @@
+package faang.school.postservice.repository;
+
+import faang.school.postservice.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+}

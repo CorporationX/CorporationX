633efa6dd020b62c9115a78f04badea82eb8b542
Roman Antonov <romzesvxo@mail.ru>
2024-05-17 20:36:00 +0300

    BJS2-6434 - Добавил зависимости в Build_gradle - Добавил аннотации в MentorshipController - Создал в config класс SwaggerConfig для конфигурации Swagger

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,6 +1,11 @@
 package school.faang.user_service.controller.mentorship;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDTO;
 import school.faang.user_service.service.mentorship.MentorshipService;
@@ -8,22 +13,32 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/mentorship")
+@Tag(name = "Mentorship", description = "Операции с менторами и менти")
 public class MentorshipController {
 
     private final MentorshipService mentorshipService;
 
+    @GetMapping("/mentees/{mentorId}")
+    @Operation(summary = "Получить список менти", description = "Возвращает список менти для указанного ментора")
     public List<UserDTO> getMentees(long mentorId) {
         return mentorshipService.getMentees(mentorId);
     }
 
+    @GetMapping("/mentors/{userId}")
+    @Operation(summary = "Получить список менторов", description = "Возвращает список менторов для указанного пользователя")
     public List<UserDTO> getMentors(long userId) {
         return mentorshipService.getMentors(userId);
     }
 
+    @DeleteMapping("/mentees/{mentorId}/{menteeId}")
+    @Operation(summary = "Удалить менти", description = "Удаляет менти от ментора")
     public void deleteMentee(long mentorId, long menteeId) {
         mentorshipService.deleteMentee(menteeId, mentorId);
     }
 
+    @DeleteMapping("/mentors/{menteeId}/{mentorId}")
+    @Operation(summary = "Удалить ментора", description = "Удаляет ментора от менти")
     public void deleteMentor(long menteeId, long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
     }

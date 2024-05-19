4685328297b8454487bb7bb5e8753d3e1d91c125
Alex Vishnevskiy <vishgioall@gmail.com>
2024-05-19 22:47:32 +0300

    добавил сваггер 📊

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     /**

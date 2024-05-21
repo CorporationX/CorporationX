dc6d1adb75cf1271d06b2c7d894fe970a0639030
Tem <tipokruto014@gmail.com>
2024-05-21 22:03:29 +0300

    изменил маппер LikeMapper

--- src/main/java/faang/school/postservice/mapper/LikeMapper.java
+++ src/main/java/faang/school/postservice/mapper/LikeMapper.java
@@ -3,13 +3,18 @@ package faang.school.postservice.mapper;
 import faang.school.postservice.dto.like.LikeDto;
 import faang.school.postservice.model.Like;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LikeMapper {
 
+    @Mapping(source = "commentId", target = "comment.id")
+    @Mapping(source = "postId", target = "post.id")
     Like toEntity(LikeDto likeDto);
 
+    @Mapping(source = "comment.id", target = "commentId")
+    @Mapping(source = "post.id", target = "postId")
     LikeDto toDto(Like like);
 
 }

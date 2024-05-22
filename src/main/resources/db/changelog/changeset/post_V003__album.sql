801e81338619aeddba63e4f367fc1a9b7aac6a38
Tem <tipokruto014@gmail.com>
2024-05-22 10:00:05 +0300

    small migration fix

--- src/main/resources/db/changelog/changeset/post_V003__album.sql
+++ src/main/resources/db/changelog/changeset/post_V003__album.sql
@@ -5,5 +5,5 @@ CREATE TABLE album_selected_users (
     album_id BIGINT NOT NULL,
     selected_user_id BIGINT NOT NULL,
     PRIMARY KEY (album_id, selected_user_id),
-    FOREIGN KEY (album_id) REFERENCES album(id)
+    CONSTRAINT fk_album_selected_users_album FOREIGN KEY (album_id) REFERENCES album (id)
 );

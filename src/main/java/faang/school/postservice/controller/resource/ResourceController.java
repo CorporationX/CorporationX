f0fb5cf8ac2fa609bf1cc13083ee4341aaa3abd8
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-05-27 19:46:44 +0300

    add swagger annotations

--- src/main/java/faang/school/postservice/controller/resource/ResourceController.java
+++ src/main/java/faang/school/postservice/controller/resource/ResourceController.java
@@ -2,6 +2,8 @@ package faang.school.postservice.controller.resource;
 
 import faang.school.postservice.dto.resource.ResourceDto;
 import faang.school.postservice.service.resource.ResourceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,10 +14,12 @@ import java.util.List;
 @RestController
 @RequestMapping("resources")
 @RequiredArgsConstructor
+@Tag(name = "Resource")
 public class ResourceController {
 
     private final ResourceService resourceService;
 
+    @Operation(summary = "Save resource")
     @PostMapping("upload/{postId}")
     public List<ResourceDto> uploadFiles(
             @PathVariable Long postId,
@@ -24,11 +28,13 @@ public class ResourceController {
         return resourceService.create(postId, files);
     }
 
+    @Operation(summary = "Download resource")
     @GetMapping("download/{key}")
     public InputStream downloadFile(@PathVariable String key) {
         return resourceService.downloadResource(key);
     }
 
+    @Operation(summary = "Delete resource")
     @DeleteMapping("{key}")
     public void deleteFile(@PathVariable String key) {
         resourceService.deleteFile(key);

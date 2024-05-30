463f2f3c59bcd820423ac1f9c00de7c13d8afa3c
SoslanDzgoev <dzgsoslan@gmail.com>
2024-05-30 12:43:58 +0400

    added global exception handler to user_service

---
+++ src/main/java/school/faang/user_service/controller/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package school.faang.user_service.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.exception.EntityNotFoundException;
+import school.faang.user_service.exception.ErrorResponse;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Entity not found exception ", e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handlerRuntimeException(RuntimeException e) {
+        log.error("Runtime exception", e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerDataValidationException(DataValidationException e) {
+        log.error("Data validation exception", e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Method argument not valid exception", e);
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), ""))
+                );
+    }
+}

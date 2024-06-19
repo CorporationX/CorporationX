e59a2914a925ad2ab28cee207614e14d4e953da6
Tem <tipokruto014@gmail.com>
2024-06-19 17:36:15 +0300

    add tests and rename migration added

--- src/test/java/faang/school/postservice/service/hashtag/HashtagServiceImplTest.java
+++ src/test/java/faang/school/postservice/service/hashtag/HashtagServiceImplTest.java
@@ -1,15 +1,23 @@
 package faang.school.postservice.service.hashtag;
 
-import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.dto.post.PostHashtagDto;
 import faang.school.postservice.mapper.PostMapper;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.HashtagRepository;
 import faang.school.postservice.service.hashtag.cache.HashtagCacheService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class HashtagServiceImplTest {
@@ -25,37 +33,72 @@ class HashtagServiceImplTest {
     private HashtagServiceImpl hashtagService;
 
     private final String hashtag1 = "hash";
+    private PostHashtagDto postDto;
     private Post post;
-    private PostDto postDto;
 
     @BeforeEach
     void init() {
-        post = Post.builder()
+        postDto = PostHashtagDto.builder()
                 .id(1L)
                 .content("#hash #tag")
                 .build();
 
-        postDto = PostDto.builder()
-                .id(2L)
+        post = Post.builder()
+                .id(1L)
                 .content("#hash #tag")
                 .build();
     }
 
-//    @Test
-//    void addHashtag() {
-//        hashtagService.addHashtag(hashtag1, post);
-//
-//        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
-//        inOrder.verify(hashtagRepository).save(any());
-//        inOrder.verify(hashtagCacheService).addPostToHashtag(hashtag1, post);
-//    }
-//
-//    @Test
-//    void deleteHashtag() {
-//        hashtagService.deleteHashtag(hashtag1, post);
-//
-//        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
-//        inOrder.verify(hashtagRepository).deleteByHashtagAndPostId(anyString(), eq(post.getId()));
-//        inOrder.verify(hashtagCacheService).removePostFromHashtag(hashtag1, post);
-//    }
+    @Test
+    void addHashtag() {
+        when(postMapper.toEntity(postDto)).thenReturn(post);
+
+        hashtagService.addHashtag(hashtag1, postDto);
+
+        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
+        inOrder.verify(hashtagRepository).save(any());
+        inOrder.verify(hashtagCacheService).addPostToHashtag(hashtag1, postDto);
+    }
+
+    @Test
+    void deleteHashtag() {
+        hashtagService.deleteHashtag(hashtag1, postDto);
+
+        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
+        inOrder.verify(hashtagRepository).deleteByHashtagAndPostId(anyString(), eq(postDto.getId()));
+        inOrder.verify(hashtagCacheService).removePostFromHashtag(hashtag1, postDto);
+    }
+
+    @Test
+    void updateHashtagExists() {
+        when(hashtagRepository.existsByHashtag(hashtag1)).thenReturn(true);
+
+        hashtagService.updateHashtag(hashtag1, postDto);
+
+        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
+        inOrder.verify(hashtagRepository).existsByHashtag(hashtag1);
+        inOrder.verify(hashtagRepository).deleteByHashtagAndPostId(anyString(), eq(postDto.getId()));
+        inOrder.verify(hashtagCacheService).removePostFromHashtag(hashtag1, postDto);
+    }
+
+    @Test
+    void updateHashtagNotExists() {
+        when(hashtagRepository.existsByHashtag(hashtag1)).thenReturn(false);
+        when(postMapper.toEntity(postDto)).thenReturn(post);
+
+        hashtagService.updateHashtag(hashtag1, postDto);
+
+        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
+        inOrder.verify(hashtagRepository).existsByHashtag(hashtag1);
+        inOrder.verify(hashtagRepository).save(any());
+        inOrder.verify(hashtagCacheService).addPostToHashtag(hashtag1, postDto);
+    }
+
+    @Test
+    void updateScore() {
+        hashtagService.updateScore(hashtag1, postDto);
+
+        InOrder inOrder = inOrder(hashtagRepository, hashtagCacheService, postMapper);
+        inOrder.verify(hashtagCacheService).updateScore(hashtag1, postDto);
+    }
 }

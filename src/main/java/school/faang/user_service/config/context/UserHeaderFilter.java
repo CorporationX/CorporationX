4e342fcc843d22faa89b18abe5ba10ae2ff58d39
Irina Vereshchagina <irankamoya@gmail.com>
2024-06-23 10:33:07 +0200

    added exception when you miss x-user-id in the request

--- src/main/java/school/faang/user_service/config/context/UserHeaderFilter.java
+++ src/main/java/school/faang/user_service/config/context/UserHeaderFilter.java
@@ -20,6 +20,8 @@ public class UserHeaderFilter implements Filter {
         String userId = req.getHeader("x-user-id");
         if (userId != null) {
             userContext.setUserId(Long.parseLong(userId));
+        }else {
+            throw new IllegalArgumentException("Missing required header 'x-user-id'. Please include 'x-user-id' header with a valid user ID in your request.");
         }
         try {
             chain.doFilter(request, response);

9893a7c90e440e5045e792f2c6cea24c5aa6f8e8
kukushkin <maxim-kukushkin65@yandex.ru>
2024-09-16 18:24:23 +0300

    Выполнил все пункты кроме написания тестов.

--- src/main/java/school/faang/user_service/service/event/EventPartcipationService.java
+++ src/main/java/school/faang/user_service/service/event/EventPartcipationService.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.service.event;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -14,18 +15,47 @@ public class EventPartcipationService {
 
     private EventParticipationRepository eventParticipationRepository;
 
+
+    @Autowired
+    public EventPartcipationService(EventParticipationRepository eventParticipationRepository){
+        this.eventParticipationRepository = eventParticipationRepository;
+    }
     // добавить конструктор класса и аннотацию
     @Bean
-    public void registerParticipant(long eventId, long userId){ // добавить в качестве параметра ивент
-        //if(userId == ) сделать проверку на то, что пользователь уже зарегистрирован на событие
-        List<User> usersOnCurrentEvent = eventParticipationRepository.findAllParticipantsByEventId(eventId); // для поиска всех участников по id события
+    public void registerParticipant(long eventId, long userId){
+        List<User> usersOnCurrentEvent = eventParticipationRepository.findAllParticipantsByEventId(eventId);
 
         for(User user : usersOnCurrentEvent){
             if(user.getId() == userId){
-                // ВЫКИНУТЬ ИСКЛЮЧЕНИЕ
-                return;
+                try {
+                    throw new Exception("Пользователь уже зарегистрирован");
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
         eventParticipationRepository.register(eventId, userId);
+    }
+    @Bean
+    public void unregisterParticipant(long userId, long eventId){
+        List<User> usersOnCurrentEvent = eventParticipationRepository.findAllParticipantsByEventId(eventId);
+        for(User user : usersOnCurrentEvent){
+            if(user.getId() == userId){
+                eventParticipationRepository.unregister(eventId, userId);
+            }
+        }
+        try {
+            throw new Exception("Пользователь не найден");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Bean
+    public List<User> getParticipant(long eventId){
+        return eventParticipationRepository.findAllParticipantsByEventId(eventId);
+    }
+    @Bean
+    public int getParticipantsCount(long eventId){
+        return eventParticipationRepository.countParticipants(eventId);
     }
 }

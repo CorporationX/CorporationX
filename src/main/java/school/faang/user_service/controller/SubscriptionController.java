479a1c5290d2312148d00ab233e57ce70ba4af5c
mighty-mhsl <mihusle@gmail.com>
2024-10-11 12:20:24 +0200

    Revert "Выполнен пункты 1-4 кроме валидациии запросов"
    This reverts commit 845c54e6c524a7103e7393d1d60de1f709922aca.

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
-    private Long id;
-    private String username;
-    private String email;
+  private Long id;
+  private String name;
+  private String email;
 }

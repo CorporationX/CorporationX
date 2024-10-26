32c8874aeb9187a4db43c78ba1bb7185477f06c4
mighty-mhsl <mihusle@gmail.com>
2024-10-26 11:45:35 +0200

    Removed student's commit

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++
@@ -1,7 +0,0 @@
-package school.faang.user_service.exception;
-
-public class DataValidationException extends Exception {
-  public DataValidationException(String message) {
-    super(message);
-  }
-}
